f3e961cf0b591c2417cf68d0b3492cabbd9ba441
unknown <luoqingxin@cmind-semi.com>
2024-06-28 17:09:38 +0800

    update git init

--- git_base_init.sh
+++ git_base_init.sh
@@ -9,3 +9,6 @@ git config --global color.diff auto
 git config --global color.branch auto
 git config --global color.interactive auto
 git config --global core.excludesfile ~/.gitignore
+
+#git clone git@github.com:HungryLQX/study study
+#git remote add origin git@github.com:HungryLQX/study
